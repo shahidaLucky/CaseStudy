4f55b10c9745601c79e93ddce8773aedf7161015
shahidaLucky <shahidakawsar@gmail.com>
2019-05-08 00:54:40 +0600

    HomePage, RegisterPage and ForgotPasswordPage component created.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import {HomePageComponent} from './home-page/home-page.component';
+import {RegisterPageComponent} from './register-page/register-page.component';
+import {ForgotPasswordPageComponent} from './forgot-password-page/forgot-password-page.component';
+
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'register', component: RegisterPageComponent },
+  { path: 'forget-password', component: ForgotPasswordPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
+import { ForgotPasswordPageComponent } from './forgot-password-page/forgot-password-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    RegisterPageComponent,
+    ForgotPasswordPageComponent
   ],
   imports: [
     BrowserModule,
